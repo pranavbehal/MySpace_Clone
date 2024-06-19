0cd4ca46da91df3b9aa0d4667966dd5654d29e74
pranavbehal <pranavbehal09@gmail.com>
2024-06-19 12:16:24 -0400

    Hour 2: Authentication, auth redirects

--- myspace/app/page.tsx
+++ myspace/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return <main className={styles.main}></main>;
 }
